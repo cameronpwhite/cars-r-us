509e06fe25a32df4b920d6ff41baf0651aa8ee2d
Cameron White <cameronpwhite98@gmail.com>
2021-07-29 10:39:59 -0500

    imported modules into cars display module

---
+++ scripts/cars-r-us.js
@@ -0,0 +1,5 @@
+//Import all display modules to be interpolated in html.
+import { Interiors } from "./Interior.js"
+import { paintColors } from "./paintColor.js"
+import { Technologies } from "./Technology.js"
+import { Wheels } from "./Wheels.js"
