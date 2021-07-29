94a5395e4635a30bab66e1db5e2b2728408c3023
Cameron White <cameronpwhite98@gmail.com>
2021-07-29 09:57:59 -0500

    added module to display html for paintColor

---
+++ scripts/paintColor.js
@@ -0,0 +1,23 @@
+//This module creates html for the paint color selection section of the site.
+{import getPaintColors from "./database.js"}
+
+const colors = getPaintColors();
+
+export const paintColors = () => {
+    let html = "<ul>"
+
+    //Use .map to create an array of list Items.
+
+    const listItems = colors.map(color => {
+        return `<li>
+            <input type="radio" name="color" value="${color.id}" /> ${color.color}
+        </li>`
+    })
+
+    html += listItems.join("")
+    html += "</ul>"
+
+    return html
+    
+}
+
